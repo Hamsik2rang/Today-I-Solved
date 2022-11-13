3aba92bb23a2dfc1aa96b839e10151368ae8c385
Im Yongsik <lvhi7121@gmail.com>
2022-11-13 20:39:39 +0900

    [Python/Graph] Solve problem 2294 "동전 2"

--- Krafton-Jungle/Week-03/2294.py
+++ Krafton-Jungle/Week-03/2294.py
@@ -5,10 +5,10 @@ from collections import deque
 def bfs(x: int, remain: int) -> int:
     check = [False for i in range(10001)]
     q = deque()
-    q.append((x, remain - x, 1))
+    q.append((remain - x, 1))
 
     while len(q) > 0:
-        cur_value, cur_remain, cur_count = q.popleft()
+        cur_remain, cur_count = q.popleft()
 
         if cur_remain == 0:
             return cur_count
